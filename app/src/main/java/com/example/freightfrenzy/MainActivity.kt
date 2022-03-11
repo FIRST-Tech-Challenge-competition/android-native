48498a4674d1e17666a13da8ad68e53d94114b1c
HenryNg101 <hungnguyenquoc8a2@gmail.com>
2022-03-11 18:30:50 +1030

    Add splash screen

--- app/src/main/java/com/example/freightfrenzy/MainActivity.kt
+++ app/src/main/java/com/example/freightfrenzy/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //Change back the theme to default home theme right before load the layout, to end the splash screen right before load into the app.
+        setTheme(R.style.Theme_FreightFrenzy)
         setContentView(R.layout.activity_main)
     }
 }
